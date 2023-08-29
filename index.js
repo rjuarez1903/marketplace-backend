7c9d8e50fd8fd8a429722a7d27bd3f89bd86c43f
Rodrigo Juarez <lic.rodrigojuarez@gmail.com>
2023-08-29 12:56:25 -0300

    fix port variable

--- index.js
+++ index.js
@@ -32,6 +32,6 @@ app.use("/api/v1/serviceContracts", serviceContractService);
 app.use("/api/v1/comments", commentRouter);
 
 const PORT = process.env.PORT || 4000;
-app.listen(4000, () => {
+app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT} http://localhost:${PORT}`);
 });
